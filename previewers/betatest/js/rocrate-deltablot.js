f18ca32b826610737739583ce8a8fc029733feaf
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2023-10-13 14:45:55 +0200

    refactoring

--- previewers/betatest/js/rocrate-deltablot.js
+++ previewers/betatest/js/rocrate-deltablot.js
@@ -1,5 +1,3 @@
-import { Builder } from 'https://unpkg.com/@deltablot/ro-crate2html@0.1.0/dist/main.js';
-
 document.querySelectorAll('[data-file]').forEach(el => {
     displayContent(el.dataset.file);
 });
@@ -13,7 +11,7 @@ function translateBaseHtmlPage() {
 }
 
 function writeContentAndData(data, fileUrl, file, title, authors) {
-    const builder = new Builder();
+    const builder = new deltablot.Builder();
     const result = builder.parse(data);
     const targetDiv = document.getElementById('ro-crate-metadata');
     result.forEach(el => targetDiv.appendChild(el));
